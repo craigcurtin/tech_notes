9bdf76c3fe66a4f7db70929baa55e9edbb85a4e6
craig.curtin <curtin@computer.org>
2023-06-30 11:09:54 -0400

    first ini file commit

---
+++ mqtt/Publisher.py
@@ -0,0 +1,14 @@
+from MQTTClass import MQTTClass
+
+
+class Publisher():
+
+    def __init__(self, host, topic):
+    self.mqtt =  host
+    self.topic = topic
+
+    def publish(self, topic, data):
+
+
+if __name__ == '__main__':
+    pub = Publisher()
