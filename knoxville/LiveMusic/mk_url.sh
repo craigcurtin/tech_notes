b65f9383ef15d7fab3e432fd95f088b663a56c01
craig.curtin <craig.curtin@bjss.com>
2023-08-25 14:22:01 -0400

    xml had space in front of string tag ... ugh

--- knoxville/LiveMusic/mk_url.sh
+++ knoxville/LiveMusic/mk_url.sh
@@ -1,14 +1,35 @@
 #!/usr/bin/env bash
 
-# for file in *.txt; do mv "$file" "${file%.txt}.py"; done
+set -euxo pipefail
 
+HEADER=/tmp/$$_Head
+FOOTER=/tmp/$$_Tail
+
+# make Header template
+cat << EOF > ${HEADER}
+[InternetShortcut]
+EOF
+
+# make footer Template
+cat << EOF > ${FOOTER}
+IconIndex=0
+EOF
+
+# for file in *.txt; do mv "$file" "${file%.txt}.py"; done
 set -x
 for MY_FILE in *.webloc; 
 do
         # cat the file, grep for string .... create new file
-        cat "$MY_FILE" | grep "<string>" | sed 's/<string>/URL=/' | sed 's/<\/string>//' > "_${MY_FILE%.webloc}.url"; 
+        BODY="_${MY_FILE%.webloc}.url"
+        TMP_BODY="/tmp/${BODY}"
+        # reminder ... XML had tab in front ...
+        cat "$MY_FILE" | grep "<string>" | sed 's/.*<string>/URL=/' | sed 's/<\/string>//' > "${TMP_BODY}"; 
+        cat "${HEADER}" "${TMP_BODY}" "${FOOTER}" > "${BODY}"
+        #rm "${TMP_BODY}"
 done
 
+#clean up
+rm "${HEADER}" "${FOOTER}"
 
 
 
