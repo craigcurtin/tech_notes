0aa2a0fba42fa0971c9a19de89fd3b9b56285ac5
craig.curtin <craig.curtin@bjss.com>
2023-08-25 14:31:00 -0400

    final cleanup of script

--- knoxville/LiveMusic/mk_url.sh
+++ knoxville/LiveMusic/mk_url.sh
@@ -2,30 +2,37 @@
 
 set -euxo pipefail
 
+# lets make a template Header and Footer here is document
 HEADER=/tmp/$$_Head
 FOOTER=/tmp/$$_Tail
 
-# make Header template
+# create Header template
 cat << EOF > ${HEADER}
 [InternetShortcut]
 EOF
 
-# make footer Template
+# create Footer Template
 cat << EOF > ${FOOTER}
 IconIndex=0
 EOF
 
 # for file in *.txt; do mv "$file" "${file%.txt}.py"; done
-set -x
+
 for MY_FILE in *.webloc; 
 do
         # cat the file, grep for string .... create new file
         BODY="_${MY_FILE%.webloc}.url"
         TMP_BODY="/tmp/${BODY}"
+
         # reminder ... XML had tab in front ...
+        # cat input file ... remove the leading and trailing string ... sub in "URL="
         cat "$MY_FILE" | grep "<string>" | sed 's/.*<string>/URL=/' | sed 's/<\/string>//' > "${TMP_BODY}"; 
+
+        # put the Header + TmpBody + Footer together 
         cat "${HEADER}" "${TMP_BODY}" "${FOOTER}" > "${BODY}"
-        #rm "${TMP_BODY}"
+
+        # clean up the tmp file ...
+        rm "${TMP_BODY}"
 done
 
 #clean up
